708f7dbef6e02169f4838cbafd46676ed0bef258
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-10-27 10:52:16 +0200

    tries $and, $regex combination
    Signed-off-by: plopezgit <p.lopez.ch.hr@hotmail.com>

--- src/queries/t4ExeU.js
+++ src/queries/t4ExeU.js
@@ -8,3 +8,4 @@ db.restaurant.find({},{_id: 0, restaurant_id: 1}).skip(5).limit(5);
 db.restaurant.find({ 'grades.score': {$gt: 90}}, {_id: 0, restaurant_id: 1, grades: {score: 1}});
 db.restaurant.find({$and: [ { 'grades.score': {$gt: 90}}, { 'grades.score': {$lt: 100}} ]}, {_id: 0, restaurant_id: 1, grades: {score: 1}});
 db.restaurant.find({ 'address.coord': {$lt: -95.754168}}, {_id: 0, restaurant_id: 1, address: {coord: 1}});
+db.restaurant.find({$and: [ {cuisine: {'$regex' : '^((?!American).)*$', '$options' : 'i'} }, { 'grades.score': {$gt: 70}}, {'address.coord': {$lt: -65.754168}}]}, {_id: 0, restaurant_id: 1, cuisine: 1, grades: {score: 1}});
